688a39d42b7fa21d39578119543461959e756e71
debcamila <camiladeborah72@gmail.com>
2019-05-28 22:05:13 -0300

    class extends ContaAbstrata ContaCc - debitar

---
+++ src/teste/ContaCc.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class ContaCc extends ContaAbstrata{
+	public ContaCc(String numero, double saldo) {
+		super(numero, saldo);
+	}
+	
+	public void debitar(double value) {
+		setSaldo(getSaldo() - value);
+	}
+}

572b2ccf3d0304104f8c5da4aab3a7fd93933050
debcamila <camiladeborah72@gmail.com>
2019-05-28 22:07:01 -0300

    class Enderenco do cliente - gets e sets

---
+++ src/teste/Endereco.java
@@ -0,0 +1,67 @@
+package teste;
+
+public class Endereco {
+	private String cidade;
+	private String estado;
+	private String bairro;
+	private String rua;
+	private String numero;
+	private String cep;
+	
+	public Endereco(String cidade, String estado, String bairro, String rua, String numero, String cep) {
+		this.cidade = cidade;
+		this.estado = estado;
+		this.bairro = bairro;
+		this.rua = rua;
+		this.numero = numero;
+		this.cep = cep;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	public String getCep() {
+		return cep;
+	}
+}

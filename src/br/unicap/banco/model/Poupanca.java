eb64e7adc8649b9ff9ab34a6578c29243ab2a89e
debcamila <camiladeborah72@gmail.com>
2019-06-10 01:27:44 -0300

    modificações na poupanca

--- src/br/unicap/banco/model/Poupanca.java
+++ src/br/unicap/banco/model/Poupanca.java
@@ -2,13 +2,15 @@ package br.unicap.banco.model;
 
 public class Poupanca extends ContaCc{
 	
-	private final double taxa = 0.01;
+	private final double TAXA = 0.01;
 	
-	public Poupanca(String numero, double saldo) {
-		super(numero, saldo);
+	public Poupanca(String numero, double saldo, Cliente cliente) {
+		super(numero, saldo, cliente);
 	}
 	
+	public Poupanca() {}
+	
 	public void renderJuros() {
-		creditar(getSaldo() * taxa);
+		creditar(getSaldo() * TAXA);
 	}
 }

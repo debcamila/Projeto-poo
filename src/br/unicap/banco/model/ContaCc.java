5cde8cb358107635f1a0e848f7d3892ce6edba95
debcamila <camiladeborah72@gmail.com>
2019-06-10 01:26:15 -0300

    modificações na conta

--- src/br/unicap/banco/model/ContaCc.java
+++ src/br/unicap/banco/model/ContaCc.java
@@ -1,11 +1,19 @@
 package br.unicap.banco.model;
 
+import exceptions.ContaSaldoInsuficienteException;
+
 public class ContaCc extends ContaAbstrata{
-	public ContaCc(String numero, double saldo) {
-		super(numero, saldo);
+	public ContaCc(String numero, double saldo, Cliente cliente) {
+		super(numero, saldo, cliente);
 	}
 	
-	public void debitar(double value) {
-		setSaldo(getSaldo() - value);
+	public ContaCc() {}
+	
+	public void debitar(double value) throws Exception {
+		if (getSaldo() >= value) {
+            setSaldo(getSaldo() - value);
+        } else {
+            throw new ContaSaldoInsuficienteException();
+        }
 	}
 }

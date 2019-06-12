af1fea1dd1332c609b757a65a93423b68a5b4362
debcamila <camiladeborah72@gmail.com>
2019-06-11 23:06:57 -0300

    modificações no main para testes

--- src/br/unicap/banco/model/TesteBanco.java
+++ src/br/unicap/banco/model/TesteBanco.java
@@ -3,9 +3,16 @@ package br.unicap.banco.model;
 public class TesteBanco {
 
 	public static void main(String[] args) {
-		ControllerContas controller = new ControllerContas();
+		Fachada fachada = Fachada.obterInstancia();
+		ContaAbstrata c1, c2;
+		Cliente cli1, cli2;
 		
 		
+		cli1 = new Cliente("Ada Lovelace", "123", 20, TipoCliente.VIP, "abc", "ba" , 125, "5353333");
+		cli2 = new Cliente("Camila Achutti", "456", 21, TipoCliente.EXECUTIVO, "def", "ed" , 123, "5353311");
+		
+		c1 = new ContaImposto("123", 100, cli1);
+		c2 = new ContaBonificada("456", 300, cli2);
 
 	}
 

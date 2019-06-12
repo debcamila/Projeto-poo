111f8a71b6761d58d482fbfec8ba2c5ca6b48df0
debcamila <camiladeborah72@gmail.com>
2019-06-11 23:03:12 -0300

    modificações no repositorio conta hashmap

--- src/br/unicap/banco/dados/RepContaMap.java
+++ src/br/unicap/banco/dados/RepContaMap.java
@@ -1,5 +1,41 @@
 package br.unicap.banco.dados;
 
-public class RepContaMap {
+import java.util.HashMap;
 
+import br.unicap.banco.model.IRepContas;
+import br.unicap.banco.model.ContaAbstrata;
+
+public class RepContaMap implements IRepContas{
+	private HashMap<String, ContaAbstrata> contas;
+	private static int CAPACIDADE = 30;
+	
+	public RepContaMap() {
+		contas = new HashMap<>(CAPACIDADE);
+	}
+	
+	@Override
+	public ContaAbstrata consultar(String num) {
+		return contas.get(num);
+	}
+	
+	public boolean existe(String num) {
+		return contas.containsKey(num);
+	}
+	
+	@Override
+	public void inserir(ContaAbstrata c) {
+		String key = c.getNumero();
+		if (!contas.containsKey(key)){
+			contas.put(key, c);
+		}
+	}
+	
+	@Override
+	public void remover(ContaAbstrata c) {
+		contas.remove(c);
+	}
+	
+	public void atualizar(ContaAbstrata c) {
+		contas.replace(c.getNumero(), c);
+	}
 }

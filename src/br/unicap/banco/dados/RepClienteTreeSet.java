7a70c10adadcba2d685a96210fd2ddd85534f6dd
debcamila <camiladeborah72@gmail.com>
2019-06-11 23:02:31 -0300

    modificações no repositorio do cliente treeset

--- src/br/unicap/banco/dados/RepClienteTreeSet.java
+++ src/br/unicap/banco/dados/RepClienteTreeSet.java
@@ -1,5 +1,51 @@
 package br.unicap.banco.dados;
 
-public class RepClienteTreeSet {
+import java.util.TreeSet;
 
+import br.unicap.banco.model.IRepClientes;
+import br.unicap.banco.model.Cliente;
+
+public class RepClienteTreeSet implements IRepClientes {
+	private TreeSet<Cliente> clientes;
+	
+	public RepClienteTreeSet() {
+		clientes = new TreeSet<>();
+	}
+	
+	@Override
+	public Cliente consultar(String cpf) {
+		// TODO Auto-generated method stub
+		for (Cliente c : clientes) {
+			if (c.getCpf().equals(cpf)) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public void inserir(Cliente c) {
+		// TODO Auto-generated method stub
+		clientes.add(c);
+	}
+	
+	@Override
+	public void remover(Cliente c) {
+		// TODO Auto-generated method stub
+		for (Cliente cli : clientes) {
+			if(cli.getCpf().equals(c.getClass())) {
+				clientes.remove(cli);
+				return;
+			}
+		}
+	}
+	
+	@Override
+	public void atualizar(Cliente c) {
+		// TODO Auto-generated method stub
+		if (clientes.contains(c)) {
+			clientes.remove(c);
+			clientes.add(c);
+		}
+	}
 }
